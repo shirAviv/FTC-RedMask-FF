59d6e4ab4d595e3799d1a2467ea85871a4a50f93
Shir Aviv-Reuven <35069152+shirAviv@users.noreply.github.com>
2022-03-11 17:47:10 +0200

    code from stemrobotics added, not used currently

--- Camera.java
+++ Camera.java
@@ -9,6 +9,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
@@ -17,6 +18,8 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.ArrayList;
 
 @TeleOp(name = "camera_1_9", group = "RedMask")
 public class Camera extends LinearOpMode {
@@ -204,7 +207,7 @@ public class Camera extends LinearOpMode {
         }
 
 
-        public String getAnalysis() {
+        public String getAnalysis1() {
             if (position_1){
                 return "LEFT";
             }else if (position_2){
@@ -215,6 +218,74 @@ public class Camera extends LinearOpMode {
                 return "NONE";
             }
             //return "RIGHT";
+        }
+
+        /* below is from stemrobotics */
+        Mat YCrCb = new Mat();
+        Mat Y = new Mat();
+        Mat RectA_Y = new Mat();
+        int avg;
+        int avgA;
+        static final int STREAM_WIDTH = 1920; // modify for your camera
+        static final int STREAM_HEIGHT = 1080; // modify for your camera
+
+
+        static final int WidthRectA = 130;
+        static final int HeightRectA = 110;
+
+        final Point RectATopLeftAnchor = new Point((STREAM_WIDTH - WidthRectA) / 2 + 300, ((STREAM_HEIGHT - HeightRectA) / 2) - 100);
+        Point RectATLCorner = new Point(
+                RectATopLeftAnchor.x,
+                RectATopLeftAnchor.y);
+        Point RectABRCorner = new Point(
+                RectATopLeftAnchor.x + WidthRectA,
+                RectATopLeftAnchor.y + HeightRectA);
+
+
+
+
+        /* This function takes the RGB frame, converts to YCrCb,
+     * and extracts the Y channel to the 'Y' variable
+     */
+        void inputToY(Mat input) {
+            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+            ArrayList<Mat> yCrCbChannels = new ArrayList<Mat>(3);
+            Core.split(YCrCb, yCrCbChannels);
+            Y = yCrCbChannels.get(0);
+
+        }
+
+        @Override
+        public void init(Mat firstFrame) {
+            inputToY(firstFrame);
+            RectA_Y = Y.submat(new Rect(RectATLCorner, RectABRCorner));
+        }
+
+//        @Override
+        public Mat processFrame1(Mat input) {
+            inputToY(input);
+            System.out.println("processing requested");
+            avg = (int) Core.mean(Y).val[0];
+            avgA = (int) Core.mean(RectA_Y).val[0];
+            YCrCb.release(); // don't leak memory!
+            Y.release(); // don't leak memory!
+
+
+            Imgproc.rectangle( // rings
+                    input, // Buffer to draw on
+                    RectATLCorner, // First point which defines the rectangle
+                    RectABRCorner, // Second point which defines the rectangle
+                    new Scalar(0,0,255), // The color the rectangle is drawn in
+                    2); // Thickness of the rectangle lines
+
+            return input;
+        }
+
+        public int getAnalysis() {
+            return avg;
+        }
+        public int getRectA_Analysis() {
+            return avgA;
         }
     }
 
